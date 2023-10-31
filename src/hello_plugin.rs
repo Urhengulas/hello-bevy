c1bdba7ed28a3bb3417a6fd3070f9fb91a0eb0f3
Johann Hemmann <johann.hemmann@code.berlin>
2023-10-31 14:26:19 +0100

    Plugins

---
+++ src/hello_plugin.rs
@@ -0,0 +1,36 @@
+use bevy::prelude::*;
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, add_people)
+            .add_systems(Update, (hello_world, greet_people));
+    }
+}
+
+// ----- COMPONENTS -----
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+// ----- SYSTEMS -----
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!!", name.0);
+    }
+}

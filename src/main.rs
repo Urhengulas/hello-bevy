f9251f1ff93957ce1e4d4bf8527cd30824c05d7f
Johann Hemmann <johann.hemmann@code.berlin>
2023-10-31 15:06:28 +0100

    Move the sprites
    https://bevyengine.org/examples/2D%20Rendering/move-sprite/

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, sprite_movement)
         .run();
+}
+
+#[derive(Component)]
+enum Direction {
+    Up,
+    Down,
 }
 
 fn setup(
@@ -17,39 +24,70 @@ fn setup(
     commands.spawn(Camera2dBundle::default());
 
     // Circle
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
-        ..default()
-    });
-
-    // Rectangle
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(50.0, 100.0)),
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
             ..default()
         },
-        transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
-        ..default()
-    });
+        Direction::Up,
+    ));
+
+    // Rectangle
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.25, 0.75),
+                custom_size: Some(Vec2::new(50.0, 100.0)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+            ..default()
+        },
+        Direction::Down,
+    ));
 
     // Quad
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes
-            .add(shape::Quad::new(Vec2::new(50., 100.)).into())
-            .into(),
-        material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
-        transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
-        ..default()
-    });
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes
+                .add(shape::Quad::new(Vec2::new(50., 100.)).into())
+                .into(),
+            material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
+            transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
+            ..default()
+        },
+        Direction::Up,
+    ));
 
     // Hexagon
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::RegularPolygon::new(50., 6).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::TURQUOISE)),
-        transform: Transform::from_translation(Vec3::new(150., 0., 0.)),
-        ..default()
-    });
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::RegularPolygon::new(50., 6).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::TURQUOISE)),
+            transform: Transform::from_translation(Vec3::new(150., 0., 0.)),
+            ..default()
+        },
+        Direction::Down,
+    ));
+}
+
+fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
+    for (mut direction, mut transform) in &mut sprite_position {
+        let y = &mut transform.translation.y;
+
+        let dy = 150.0 * time.delta_seconds();
+        match *direction {
+            Direction::Up => *y += dy,
+            Direction::Down => *y -= dy,
+        }
+
+        // bounce at the edges
+        if *y > 200.0 {
+            *direction = Direction::Down;
+        } else if *y < -200.0 {
+            *direction = Direction::Up;
+        }
+    }
 }

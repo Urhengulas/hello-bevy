5dcc3a0c1c08d3fb20d8cbc6709eed8b14057a30
Johann Hemmann <johann.hemmann@code.berlin>
2023-10-31 14:34:22 +0100

    Resources

--- src/hello_plugin.rs
+++ src/hello_plugin.rs
@@ -4,7 +4,8 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
             .add_systems(Update, (hello_world, greet_people));
     }
 }
@@ -29,8 +30,15 @@ fn hello_world() {
     println!("hello world!");
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!!", name.0);
+        }
     }
 }
+
+// ----- RESOURCES -----
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
